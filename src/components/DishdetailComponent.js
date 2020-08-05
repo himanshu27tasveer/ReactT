de2b059344b31d5c23585da0d6314cdbb3ba9dae
Himanshu Mahawar <himanshu27.stU@gmail.com>
2020-08-05 18:22:51 +0530

    Converted to functional components in week2 les than halway

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,71 +1,80 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {
     Card, CardImg, CardText, CardBody,
     CardTitle
 } from 'reactstrap';
 
 
-class DishDetail extends Component {
-    constructor(props) {
-        super(props);
-    }
 
 
-    renderDish(dish) {
-        if (dish != null)
-            return (
-                <Card>
-                    <CardImg top src={dish.image} alt={dish.name} />
-                    <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
-            );
-        else
-            return (
-                <div></div>
-            );
-    }
-
-    renderComments(dish) {
-        if (dish != null) {
-            const commentlist = dish.comments.map(item => {
-                return (
-                    <div className="container">
-                        <div key={item.id}>
-                            <p>{item.comment}
-                                <br />
-                                {"-- " + item.author + ", " + item.date}
-                            </p>
-                        </div>
-                    </div>
-                );
-            });
-            return commentlist;
-        }
-        else {
-            return (<div></div>);
-        }
-    }
-
-
-
-    render() {
-
+function renderDish(dish) {
+    if (dish != null)
         return (
-            <div className="row">
-                <div className="col-12 col-md-5 m-1">
-                    {this.renderDish(this.props.dish)}
-                </div>
-                <div className="col-12 col-md-5 m-1 container">
-                    {(this.props.dish) ? <h4 className="container">Comments</h4> : ""}
-                    {this.renderComments(this.props.dish)}
-                </div>
-            </div>
+            <Card >
+                <CardImg top src={dish.image} alt={dish.name} />
+                <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+                </CardBody>
+            </Card>
         );
+    else
+        return (
+            <div></div>
+        );
+}
+
+
+
+function readDate(date) {
+    const options = { year: 'numeric', month: 'short', day: '2-digit' }
+    const dt = new Intl.DateTimeFormat('en-in', options);
+    return dt.format(new Date(Date.parse(date)))
+}
+
+
+
+
+function renderComments(dish) {
+    if (dish != null) {
+        const commentlist = dish.comments.map(item => {
+            return (
+                <div>
+                    <div key={item.id}>
+                        <p>{item.comment}
+                            <br />
+                            {"-- " + item.author + ", " + readDate(item.date)}
+                        </p>
+                    </div>
+                </div>
+            );
+        });
+        return commentlist;
+    }
+    else {
+        return (<div></div>);
     }
 }
+
+
+
+
+const DishDetail = (props) => {
+
+    return (
+        <div className="row">
+            <div className="col-12 col-md-5 m-1">
+                {renderDish(props.dish)}
+            </div>
+            <div className="col-12 col-md-5 m-1">
+                {(props.dish) ? <h4>Comments</h4> : ""}
+                {renderComments(props.dish)}
+            </div>
+        </div>
+    );
+}
+
+
 
 
 export default DishDetail;
---
+++ src/components/MainComponent.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './MenuComponent';
+import DishDetail from './DishdetailComponent'
+import { DISHES } from '../shared/dishes';
+
+
+
+
+class Main extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            dishes: DISHES,
+            selectedDish: null
+        };
+    }
+
+    onDishSelect(dishID) {
+        this.setState({ selectedDish: dishID });
+    }
+
+    render() {
+        return (
+            <div className="Main">
+                <Navbar dark color="primary">
+                    <div className="container">
+                        <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+                    </div>
+                </Navbar>
+                <Menu dishes={this.state.dishes} onClick={(dishID) => this.onDishSelect(dishID)} />
+                <div className="container">
+                    <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+                </div>
+
+            </div>
+        );
+    }
+}
+
+
+
+export default Main;
+
